31b3176237ac497d328f1044f7afeaf6e9b55da7
StarkOdinson612 <darkknightishaan@gmail.com>
2021-11-20 18:05:32 -0500

    commit

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
     name="mee6_graphing_lib",  # How you named your package folder (MyLib)
     packages=["mee6_graphing_lib"],  # Cshosse the same as "name"
-    version="0.5",  # Start with a small number and increase it with every change you make
+    version="0.5.1",  # Start with a small number and increase it with every change you make
     license="MIT",  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description="A library that tracks user progress in the MEE6 discord bot",  # Give a short description about your library
     author="Stark Odinson",  # Type in your name
     author_email="darkknightishaan@gmail.com",  #  # Type in your E-Mail
     url="https://github.com/StarkOdinson612/mee6_graphing_lib",  # Provide either the link to your github or to your website
-    download_url="https://github.com/StarkOdinson612/mee6_graphing_lib/archive/refs/tags/v0.5.tar.gz",  # I explain this later on
+    download_url="https://github.com/StarkOdinson612/mee6_graphing_lib/archive/refs/tags/v0.5.1.tar.gz",  # I explain this later on
     keywords=[
         "MEE6",
         "Graphing",
