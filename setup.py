12e2900bc7e4a65168e56c7cd6c04d878b2dde00
StarkOdinson612 <darkknightishaan@gmail.com>
2021-11-20 12:09:05 -0500

    Updated setup file

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name="mee6_graphing_lib",  # How you named your package folder (MyLib)
     packages=["mee6_graphing_lib"],  # Cshosse the same as "name"
-    version="0.4.5",  # Start with a small number and increase it with every change you make
+    version="0.5",  # Start with a small number and increase it with every change you make
     license="MIT",  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description="A library that tracks user progress in the MEE6 discord bot",  # Give a short description about your library
     author="Stark Odinson",  # Type in your name
@@ -16,7 +16,11 @@ setup(
         "Discord",
         "Levelling",
     ],  # Keywords that define your package best
-    install_requires=["mee6-py-api", "matplotlib"],  # I get to this in a second
+    install_requires=[
+        "mee6-py-api",
+        "matplotlib",
+        "numpy",
+    ],  # I get to this in a second
     classifiers=[
         "Development Status :: 4 - Beta",  # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
         "Intended Audience :: Developers",  # Define that your audience are developers
